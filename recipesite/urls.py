fe47d8ba7a7621b4b2cc01ea4d0ec156ad9f88b2
SanneLaan <sannevdlaan25@gmail.com>
2017-01-09 17:53:47 +0100

    Add files via upload

---
+++ recipesite/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+from companies import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+	url(r'^recipes/', include('recipes.urls')),
+	url(r'^stocks/', views.StockList.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
